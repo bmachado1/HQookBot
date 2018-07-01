3d3d6c7d058469697220ec90789d7d0d7c3e52bf
Epiphic <zinkofficialy@gmail.com>
2018-06-30 23:42:13 -0700

    uodates
    working on doing ratio between just the answer and the questions + answer

--- hq.py
+++ hq.py
@@ -23,9 +23,19 @@ def getText(filename):
 	text = image_to_string(img)
 	return text
 
-def get_results(question, answer):
+def get_results_with_question(question, answer):
 	concatinate = answer + " " + question  
 	re = requests.get('https://www.google.com/search', params={'q':concatinate})
+	soup = bs(re.text, "html.parser")
+	response = soup.find('div', {'class': 'sd'})
+	number = ""
+	for char in range(len(response.text)):
+		if response.text[char].isdigit():
+			number+=str(response.text[char])
+	return number
+
+def get_results(answer):
+	re = requests.get('https://www.google.com/search', params={'q':answer})
 	soup = bs(re.text, "html.parser")
 	response = soup.find('div', {'class': 'sd'})
 	number = ""
@@ -44,6 +54,20 @@ def checkEasy(question):
 		if len(i.text)>0:
 			return i.text[0:20]
 
+def only_results(ra1,ra2,ra3):
+	if ra1>ra2 and ra1>ra3:
+		print "1"
+
+	if ra2>ra1 and ra2>ra3:
+		print "2"
+
+	if ra3>ra1 and ra3>ra2:
+		print "3"
+
+# def results_ratio(rqa1,rqa2,rqa3,ra1,ra2,ra3):
+
+
+
 
 	
 if __name__ == '__main__':
@@ -53,20 +77,25 @@ if __name__ == '__main__':
 	getSS(750,425,1150,500,"hqA2.png", False)
 	getSS(750,500,1150,575,"hqA3.png", False)
 
-	print checkEasy(getText("hqQ.png"))
+	qtext = getText("hqQ.png")
+	a1text = getText("hqA1.png")
+	a2text = getText("hqA2.png")
+	a3text = getText("hqA3.png")
 
-	a1 =  get_results(getText("hqQ.png"), getText("hqA1.png"))
-	a2 =  get_results(getText("hqQ.png"), getText("hqA2.png"))
-	a3 =  get_results(getText("hqQ.png"), getText("hqA3.png"))
+	print checkEasy(qtext)
 
-	if a1>a2 and a1>a3:
-		print "1"
+	qa1_results =  get_results_with_question(qtext, a1text)
+	qa2_results =  get_results_with_question(qtext, a2text)
+	qa3_results =  get_results_with_question(qtext, a3text)
 
-	if a2>a1 and a2>a3:
-		print "2"
+	a1_results =  get_results(a1text)
+	a2_results =  get_results(a2text)
+	a3_results =  get_results(a3text)
 
-	if a3>a1 and a3>a2:
-		print "3"
+	only_results(qa1_results,qa2_results,qa3_results)
+
+	# results_ratio(a1,a2,a3)
+
 	getTime("Done")
 
 
